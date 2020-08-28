c1ad4f4f830777609c0cb223630eaa3a49ef3442
Tyler Jones <tjjones93@gmail.com>
2020-08-28 18:13:55 -0400

    style: dashboard and homepage

--- public/js/index.js
+++ public/js/index.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
     $('.toggle-dark-mode').click(function () {
         $('body').toggleClass('bg-light dark-mode');
-        $('nav a').toggleClass('nav-light');
+        $('a').toggleClass('nav-light');
         $('i').toggleClass('social-light');
-        $('p a').toggleClass('a-light')
+        $('p a').toggleClass('a-light');
         if ($('body').hasClass('bg-light')) {
             $('.nav-btn').attr('src', 'images/moon-32x32.png');
         } else {
