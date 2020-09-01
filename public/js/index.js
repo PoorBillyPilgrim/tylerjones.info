1801f3c02aaa07ccb82415f825e6ff9de59b528b
Tyler Jones <tjjones93@gmail.com>
2020-09-01 17:50:31 -0400

    style: notes homepage

--- public/js/index.js
+++ public/js/index.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     $('.toggle-dark-mode').click(function () {
         $('body').toggleClass('bg-light dark-mode');
         $('.projects-card').toggleClass('projects-card-dark projects-card-light');
+        $('.notes-card').toggleClass('notes-card-dark notes-card-light');
         $('a').toggleClass('nav-light');
         $('i').toggleClass('social-light');
         $('p a').toggleClass('a-light');
